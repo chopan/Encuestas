22afcb1c21324134cd9ad32400e677e36e4a2733
Rolando Vazquez Hernandez <rolando@rolando-Inspiron-N5110.(none)>
2011-09-21 14:40:37 -0600

    Se crea accion new de encuestas con artibutos anidados

--- app/controllers/encuestas_controller.rb
+++ app/controllers/encuestas_controller.rb
@@ -1,12 +1,27 @@
 class EncuestasController < ApplicationController
 
+ def index
+   @encuestas = Encuesta.all
+ end
+
 def new
   @encuesta = Encuesta.new
   3.times do
-    pregunta = @encuesta.pregunta.build
-    3.times { preguntas.respuesta.build }
+    pregunta = @encuesta.preguntas.build
+    3.times { pregunta.respuestas.build }
   end
 end
 
-
+def create
+  @encuesta = Encuesta.new(params[:encuesta])
+  @encuesta.preguntas.each do |pregunta|
+      pregunta.encuesta_id = @encuesta.id
+  end
+  if @encuesta.save
+    flash[:notice] = "Encuesta guardada correctamente"
+    redirect_to encuestas_path
+  else
+    render 'new'
+  end
+end
 end
--- app/models/encuesta.rb
+++ app/models/encuesta.rb
@@ -2,6 +2,9 @@ class Encuesta < ActiveRecord::Base
   belongs_to :creador, :class_name => "Usuario"
   has_many :temas
 
-has_many :preguntas, :dependent => :destroy
-  accepts_nested_attributes_for :preguntas, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  has_many :preguntas, :dependent => :destroy
+
+  accepts_nested_attributes_for :preguntas, :reject_if => lambda { |a| a[:texto].blank? }, :allow_destroy => true
 end
+
+
