6b802d8be1e9f6261587e32f6db22b50691f7caf
Rolando Vazquez Hernandez <rolando@rolando-Inspiron-N5110.(none)>
2011-09-23 12:47:50 -0600

    se eliminan archivos de repositorio

--- db/migrate/20110920163454_create_usuario_sesions.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsuarioSesions < ActiveRecord::Migration
-  def change
-    create_table :usuario_sesions do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20110920192805_create_respuesta.rb
+++
@@ -1,9 +0,0 @@
-class CreateRespuesta < ActiveRecord::Migration
-  def change
-    create_table :respuesta do |t|
-	t.integer :pregunta_id
-	t.string :contenido
-      t.timestamps
-    end
-  end
-end
